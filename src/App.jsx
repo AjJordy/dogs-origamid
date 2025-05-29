dbc4a246993b646d2540a9a8f8e3675b2628eaea
AjJordy <jordyfaria0@gmail.com>
2025-05-29 09:34:54 -0300

    feat: create and login form

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Login from "./components/login/Login";
+import { UserStorage } from "./context/UserContext";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
